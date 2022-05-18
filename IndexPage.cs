0bce2d09e00b1e41127dedc964d758e580450753
kulbokasr <kulbokasr@gmail.com>
2022-05-18 21:18:16 +0300

    Initial commit

---
+++ IndexPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketApp
+{
+    public static class IndexPage
+    {
+        public static string Header = "HTTP/1.1 200 OK\r\n" + "Connection: close\r\n\r\n";
+        public static string Body = "<html><body>This is index page</body></html>";
+
+        public static string GetIndexPage()
+        {
+            return Header + Body;
+        }
+    }
+
+}
